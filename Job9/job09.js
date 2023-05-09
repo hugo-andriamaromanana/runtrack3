bf0602c9932af6582d89d681bb0362e8f0cd5b5b
hugo.andriamaromanana@laplateforme.io <hugo.andriamaromanana@laplateforme.io>
2023-05-09 11:22:31 +0200

    [FIX] Job09

--- Job9/job09.js
+++ Job9/job09.js
@@ -1,8 +1,3 @@
-// Créez une fonction “tri” qui prend en paramètres un tableau de nombres nommé
-// “numbers” et une variable “order” qui contient “asc” ou “desc”. A l’aide d’un algorithme
-// développé par vos soins, l’algorithme doit trier le tableau dans l’ordre ascendant ou
-// décroissant, selon le paramètre passé, puis retourner le tableau.
-
 function tri(tableau,order) {
     if (order == "asc") {
         tableau.sort(function(a, b){return a - b});
